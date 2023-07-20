af8fadad65122a5e227609733e281f4a0d40d70b
letuanvu <ngovipmo1@gmail.com>
2023-07-20 10:36:47 +0700

    1

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
   })
 
 app.get('/', (req, res) => {
-    // res.sendFile(__dirname + 'index.html')
+    res.sendFile(__dirname + 'index.html')
     res.json({ message: JSON.stringify(server)})
 })
 
